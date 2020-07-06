cd64058ebc70660f1781d34d2c1a483abeccbfe9
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-07-06 16:45:11 -0300

    Aprendendo Funções

---
+++ Arrays e Objetos/01_push_pop.js
@@ -0,0 +1,10 @@
+// Adicionar(push) ou remover (pop) elementos do fim do Array
+let nomes = ["Matheus", "Maria", "José"];
+
+let elementoRemovido = nomes.pop();
+
+console.log(elementoRemovido);
+console.log(nomes);
+
+nomes.push('Rafael');
+console.log(nomes);
---
+++ Arrays e Objetos/02_shift_unshift.js
@@ -0,0 +1,10 @@
+// Adicionar(push) ou remover (pop) elementos do fim do Array
+let nomes = ["Matheus", "Maria", "José"];
+
+let elementoRemovido = nomes.shift();
+
+console.log(elementoRemovido);
+console.log(nomes);
+
+nomes.unshift('Rafael');
+console.log(nomes);
