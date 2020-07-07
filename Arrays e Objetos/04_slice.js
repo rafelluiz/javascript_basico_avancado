cc22db488da3a035d545f3b5afff3499f434298b
Rafael Luiz de Araújo Oliveira <rafelluiz@gmail.com>
2020-07-07 11:51:47 -0300

    Exercicios indexOf, LastindeOf , slice e forEach

---
+++ Arrays e Objetos/04_slice.js
@@ -0,0 +1,7 @@
+let nums = [5,6,2,4,9,6,2];
+
+console.log(nums.slice(3));
+
+console.log(nums.slice(2,3));
+
+console.log(nums.slice(-2,-1));
---
+++ Arrays e Objetos/05_forEach.js
@@ -0,0 +1,7 @@
+let nums = [5,6,2,4,9,6,2];
+
+nums.forEach(numero => {
+  console.log(`Número: ${numero}`);
+});
+
+
